a306a17200fc5a502e6aedb9b6852ddd83771ab8
David Strauch <dmaster18@gmail.com>
2019-10-25 19:15:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,7 +46,7 @@ class Dog
     dog = Dog.create(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
   end
   
-  def self.find_or_create_by(name:, breed:)
+  def self.find_or_create_by(id: nil, name:, breed:)
       dog_array = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
       if dog_array != nil
         dog_data = dog_array[0]
