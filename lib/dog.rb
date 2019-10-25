b1a4426872f7f8c1073461f34160a2e87ef774b5
David Strauch <dmaster18@gmail.com>
2019-10-25 19:01:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -34,8 +34,8 @@ class Dog
     dog = Dog.create(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
   end  
   
-  def self.create(name:, breed:)
-    dog = Dog.new(name: name, breed: breed)
+  def self.create(id:, name:, breed:)
+    dog = Dog.new(id: id, name: name, breed: breed)
     dog.save
     dog
   end
